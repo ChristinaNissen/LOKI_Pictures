923dc5b10f38fe023b29fa365af838bf5c6bf191
Christina Nissen <christinaf.nissen@hotmail.com>
2025-12-19 13:16:57 +0100

    Updae the images to display in visual selection

--- src/Components/VisualSelection_Picture.js
+++ src/Components/VisualSelection_Picture.js
@@ -7,110 +7,109 @@ import "./VisualSelection_Picture.css";
 import { saveCorrectSelections, getVisualRepresentation, saveBallotSelections } from '../API/Voter.js'; // import getVisualRepresentation
 
 // Import your images
-import img4 from "../Images/alligator.jpg";
-import img5 from "../Images/alpaca.jpg";
-import img6 from "../Images/Apples.png";
-import img7 from "../Images/aquarium.jpg";
-import img8 from "../Images/baby.jpg";
-import img9 from "../Images/bacon.jpg";
-import img10 from "../Images/ball.jpg";
-import img11 from "../Images/balloon.jpg";
-import img12 from "../Images/banana.jpg";
-import img13 from "../Images/basketball.jpg";
-import img14 from "../Images/beachball.jpg";
-import img15 from "../Images/bead.jpg";
-import img16 from "../Images/beanie.jpg";
-import img17 from "../Images/bear.jpg";
-import img18 from "../Images/beer.jpg";
-import img19 from "../Images/bat.jpg";
-import img20 from "../Images/bee.jpg";
-import img21 from "../Images/beetle.jpg";
-import img22 from "../Images/bell_pepper.jpg";
-import img23 from "../Images/berry.jpg";
-import img24 from "../Images/birthday.jpg";
-import img25 from "../Images/bouquet.jpg";
-import img26 from "../Images/bow.jpg";
-import img27 from "../Images/bowl.jpg";
-import img28 from "../Images/broccoli.jpg";
-import img29 from "../Images/butterfly.jpg";
-import img30 from "../Images/calf.jpg";
-import img31 from "../Images/camera.jpg";
-import img32 from "../Images/candle.jpg";
-import img33 from "../Images/candy_cane.jpg";
-import img34 from "../Images/candy.jpg";
-import img35 from "../Images/carousel.jpg";
-import img36 from "../Images/carriage.jpg";
-import img37 from "../Images/carrot.jpg";
-import img38 from "../Images/cat.jpg";
-import img39 from "../Images/caviar.jpg";
-import img40 from "../Images/cereal.jpg";
-import img41 from "../Images/champagne.jpg";
-import img42 from "../Images/cheeseburger.jpg";
-import img43 from "../Images/chick.jpg";
-import img44 from "../Images/chicken.jpg";
-import img45 from "../Images/chips.jpg";
-import img46 from "../Images/christmas_tree.jpg";
-import img47 from "../Images/cobra.jpg";
-import img48 from "../Images/coffee.jpg";
-import img49 from "../Images/confetti.jpg";
-import img50 from "../Images/crab.jpg";
-import img51 from "../Images/crow.jpg";
-import img52 from "../Images/crepe.jpg";
-import img53 from "../Images/cucumber.jpg";
-import img54 from "../Images/cushion.jpg";
-import img55 from "../Images/dart.jpg";
-import img56 from "../Images/deodorant.jpg";
-import img57 from "../Images/dice.jpg";
-import img58 from "../Images/dog.jpg";
-import img59 from "../Images/dolphin.jpg";
-import img60 from "../Images/dough.jpg";
-import img61 from "../Images/eagle.jpg";
-import img62 from "../Images/egg.jpg";
-import img63 from "../Images/ear.jpg";
-import img64 from "../Images/earring.jpg";
-import img65 from "../Images/elephant.jpg";
-import img66 from "../Images/eye.jpg";
-import img67 from "../Images/face.jpg";
-import img68 from "../Images/feather.jpg";
-import img69 from "../Images/finger.jpg";
-import img70 from "../Images/fire.jpg";
-import img71 from "../Images/fireworks.jpg";
-import img72 from "../Images/foot.jpg";
-import img73 from "../Images/fox.jpg";
-import img74 from "../Images/frog.jpg";
-import img75 from "../Images/garlic.jpg";
-import img76 from "../Images/gift.jpg";
-import img77 from "../Images/gingerbread_man.jpg";
-import img78 from "../Images/giraffe.jpg";
-import img79 from "../Images/girl.jpg";
-import img80 from "../Images/globe.jpg";
-import img81 from "../Images/gorilla.jpg";
-import img82 from "../Images/hand.jpg";
-import img83 from "../Images/honey.jpg";
-import img84 from "../Images/hotair_balloon.jpg";
-import img85 from "../Images/hourglass.jpg";
-import img86 from "../Images/ice_cream.jpg";
-import img87 from "../Images/iceskate.jpg";
-import img88 from "../Images/jam.jpg";
-import img89 from "../Images/javelin.jpg";
-import img90 from "../Images/jetski.jpg";
-import img91 from "../Images/kayak.jpg";
-import img92 from "../Images/key.jpg";
-import img93 from "../Images/kimono.jpg";
-import img94 from "../Images/ladybug.jpg";
-import img95 from "../Images/lamb.jpg";
+import img1 from "../Images/alligator.jpg";
+import img2 from "../Images/alpaca.jpg";
+import img3 from "../Images/apple.jpg";
+import img4 from "../Images/Apples.png";
+import img5 from "../Images/aquarium.jpg";
+import img6 from "../Images/avocado.jpg";
+import img7 from "../Images/balloon.jpg";
+import img8 from "../Images/banana.jpg";
+import img9 from "../Images/basketball.jpg";
+import img10 from "../Images/bear.jpg";
+import img11 from "../Images/beard.jpg";
+import img12 from "../Images/bouquet.jpg";
+import img13 from "../Images/bow.jpg";
+import img14 from "../Images/bowl.jpg";
+import img15 from "../Images/broccoli.jpg";
+import img16 from "../Images/bubble.jpg";
+import img17 from "../Images/butterfly.jpg";
+import img18 from "../Images/calf.jpg";
+import img19 from "../Images/camera.jpg";
+import img20 from "../Images/carousel.jpg";
+import img21 from "../Images/carrot.jpg";
+import img22 from "../Images/cat.jpg";
+import img23 from "../Images/champagne.jpg";
+import img24 from "../Images/cheeseburger.jpg";
+import img25 from "../Images/christmas_tree.jpg";
+import img26 from "../Images/coffee.jpg";
+import img27 from "../Images/cucumber.jpg";
+import img28 from "../Images/cushion.jpg";
+import img29 from "../Images/dart.jpg";
+import img30 from "../Images/deodorant.jpg";
+import img31 from "../Images/dice.jpg";
+import img32 from "../Images/dog.jpg";
+import img33 from "../Images/dolphin.jpg";
+import img34 from "../Images/dough.jpg";
+import img35 from "../Images/dough_01b.jpg";
+import img36 from "../Images/eagle.jpg";
+import img37 from "../Images/ear.jpg";
+import img38 from "../Images/earring.jpg";
+import img39 from "../Images/egg.jpg";
+import img40 from "../Images/elephant.jpg";
+import img41 from "../Images/envelope_04s.jpg";
+import img42 from "../Images/eye.jpg";
+import img43 from "../Images/face.jpg";
+import img44 from "../Images/feather.jpg";
+import img45 from "../Images/finger.jpg";
+import img46 from "../Images/fire.jpg";
+import img47 from "../Images/fireworks.jpg";
+import img48 from "../Images/foot.jpg";
+import img49 from "../Images/fox.jpg";
+import img50 from "../Images/frog.jpg";
+import img51 from "../Images/garlic.jpg";
+import img52 from "../Images/gift.jpg";
+import img53 from "../Images/gingerbread_man.jpg";
+import img54 from "../Images/giraffe.jpg";
+import img55 from "../Images/girl.jpg";
+import img56 from "../Images/globe.jpg";
+import img57 from "../Images/gorilla.jpg";
+import img58 from "../Images/hand.jpg";
+import img59 from "../Images/highlighter_01b.jpg";
+import img60 from "../Images/honey.jpg";
+import img61 from "../Images/hoodie_01b.jpg";
+import img62 from "../Images/horse.jpg";
+import img63 from "../Images/hotair_balloon.jpg";
+import img64 from "../Images/hourglass.jpg";
+import img65 from "../Images/ice_cream.jpg";
+import img66 from "../Images/iceskate.jpg";
+import img67 from "../Images/inhaler_02s.jpg";
+import img68 from "../Images/ink_01b.jpg";
+import img69 from "../Images/insole_04s.jpg";
+import img70 from "../Images/iron_01b.jpg";
+import img71 from "../Images/jacket_02s.jpg";
+import img72 from "../Images/jalapeno_01b.jpg";
+import img73 from "../Images/jam.jpg";
+import img74 from "../Images/javelin.jpg";
+import img75 from "../Images/jelly_bean_01b.jpg";
+import img76 from "../Images/jetski.jpg";
+import img77 from "../Images/juice_03s.jpg";
+import img78 from "../Images/kangaroo_01b.jpg";
+import img79 from "../Images/kayak.jpg";
+import img80 from "../Images/ketchup_01b.jpg";
+import img81 from "../Images/key.jpg";
+import img82 from "../Images/key_01b.jpg";
+import img83 from "../Images/keyboard_01s.jpg";
+import img84 from "../Images/kimono.jpg";
+import img85 from "../Images/ladybug.jpg";
+import img86 from "../Images/lamb.jpg";
+import img87 from "../Images/lantern_01b.jpg";
+import img88 from "../Images/lasagna_03s.jpg";
+import img89 from "../Images/lego_01b.jpg";
+import img90 from "../Images/lemon_01b.jpg";
+import img91 from "../Images/limousine_01s.jpg";
 
 const allImages = [
-  img4, img5, img6, img7, img8, img9, img10, img11, img12, img13,
-  img14, img15, img16, img17, img18, img19, img20, img21, img22, img23,
-  img24, img25, img26, img27, img28, img29, img30, img31, img32, img33,
-  img34, img35, img36, img37, img38, img39, img40, img41, img42, img43,
-  img44, img45, img46, img47, img48, img49, img50, img51, img52, img53,
-  img54, img55, img56, img57, img58, img59, img60, img61, img62, img63,
-  img64, img65, img66, img67, img68, img69, img70, img71, img72, img73,
-  img74, img75, img76, img77, img78, img79, img80, img81, img82, img83,
-  img84, img85, img86, img87, img88, img89, img90, img91, img92, img93,
-  img94, img95
+  img1, img2, img3, img4, img5, img6, img7, img8, img9, img10,
+  img11, img12, img13, img14, img15, img16, img17, img18, img19, img20,
+  img21, img22, img23, img24, img25, img26, img27, img28, img29, img30,
+  img31, img32, img33, img34, img35, img36, img37, img38, img39, img40,
+  img41, img42, img43, img44, img45, img46, img47, img48, img49, img50,
+  img51, img52, img53, img54, img55, img56, img57, img58, img59, img60,
+  img61, img62, img63, img64, img65, img66, img67, img68, img69, img70,
+  img71, img72, img73, img74, img75, img76, img77, img78, img79, img80,
+  img81, img82, img83, img84, img85, img86, img87, img88, img89, img90,
+  img91
 ];
 
 const PAGE_SIZE = 40;
@@ -132,11 +131,11 @@ const VisualSelectionPicture = () => {
   // Shuffle the images to randomize order
   let shuffledImages = shuffleArray(allImages);
 
-  // Take the first 48 images and ensure alpaca (img5) is always included
+  // Take the first 48 images and ensure alpaca (img2) is always included
   let initialImages = shuffledImages.slice(0, 48);
-  if (!initialImages.includes(img5)) {
+  if (!initialImages.includes(img2)) {
     const randomIdx = Math.floor(Math.random() * initialImages.length);
-    initialImages[randomIdx] = img5;
+    initialImages[randomIdx] = img2;
     // Optionally, reshuffle the subset to further randomize order:
     initialImages = shuffleArray(initialImages);
   }
